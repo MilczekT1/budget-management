902dc034b9517c2a2bd645cb30b68f07f7130ee3
Konrad Boniecki <konrad_boniecki@hotmail.com>
2022-06-02 00:26:57 +0200

    Prepare next release budget-management 0.8.0-SNAPSHOT
    Feature/bud 106 GitHub consistency (#4)
    
    * BUD-106: Run AC tests just after deployment
    
    * BUD-106: changed docker base image -> java-base-image-latest
    
    * BUD-106: AC config-server config-server-0.5.0-SNAPSHOT-332 -> config-server-service-0.7.1
    
    * BUD-106: Remove deploy-to-nexus-approval
    
    * BUD-106: Remove approve_deployment
    
    * BUD-106: Analyze on SonarCloud concurrently
    
    * BUD-106: added encrypt key
    
    BUD-115: Fixed plenty of issues detected by sonar
    
    BUD-124: Missing contracts (already added in contract repository)

--- budget-management-app/src/main/java/pl/konradboniecki/budget/budgetmanagement/controller/BudgetController.java
+++ budget-management-app/src/main/java/pl/konradboniecki/budget/budgetmanagement/controller/BudgetController.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.konradboniecki.budget.budgetmanagement.feature.budget.BudgetService;
 import pl.konradboniecki.budget.openapi.api.BudgetManagementApi;
@@ -12,13 +11,9 @@ import pl.konradboniecki.budget.openapi.dto.model.OASBudget;
 import pl.konradboniecki.budget.openapi.dto.model.OASBudgetCreation;
 import pl.konradboniecki.budget.openapi.dto.model.OASCreatedBudget;
 
-import static pl.konradboniecki.budget.budgetmanagement.controller.BudgetController.BASE_PATH;
-
 @AllArgsConstructor
 @RestController
-@RequestMapping(value = BASE_PATH)
 public class BudgetController implements BudgetManagementApi {
-    public static final String BASE_PATH = "/api/budget-mgt/v1";
 
     private final BudgetService budgetService;
 
