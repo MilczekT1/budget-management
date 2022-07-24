2e63d402eb5df90da49291fe90f3b8e6ae98a070
Konrad Boniecki <konrad_boniecki@hotmail.com>
2022-07-24 12:42:26 +0200

    trying out

--- budget-management-app/src/test/java/pl/konradboniecki/budget/budgetmanagement/cucumber/commons/Config.java
+++ budget-management-app/src/test/java/pl/konradboniecki/budget/budgetmanagement/cucumber/commons/Config.java
@@ -1,6 +1,8 @@
 package pl.konradboniecki.budget.budgetmanagement.cucumber.commons;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -9,6 +11,8 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,13 +38,28 @@ public class Config {
 
     @Bean
     @Profile("acceptance-tests")
-    public TestRestTemplate testRestTemplateOnDeployment() {
+    public TestRestTemplate testRestTemplateOnDeployment(final ClientHttpRequestFactory clientHttpRequestFactory) {
         log.info("SETUP -> Initializing TestRestTemplate on deployment");
         String rootUri = acceptanceTestsProperties.getBaseUrl();
         assertThat(rootUri).isNotNull();
 
-        TestRestTemplate testRestTemplate = new TestRestTemplate(new RestTemplateBuilder().rootUri(rootUri));
+        final var testRestTemplateBuilder = new RestTemplateBuilder()
+                .requestFactory(() -> clientHttpRequestFactory)
+                .rootUri(rootUri).additionalInterceptors();
+
+        TestRestTemplate testRestTemplate = new TestRestTemplate(testRestTemplateBuilder);
         testRestTemplate.getRestTemplate().getInterceptors().add(new CucumberInterceptor());
         return testRestTemplate;
+    }
+
+    @Bean
+    @Profile("acceptance-tests")
+    public ClientHttpRequestFactory clientHttpRequestFactory() {
+        final var httpClient = HttpClients.custom()
+                .setSSLHostnameVerifier(new NoopHostnameVerifier())
+                .build();
+        final var requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+        return requestFactory;
     }
 }
