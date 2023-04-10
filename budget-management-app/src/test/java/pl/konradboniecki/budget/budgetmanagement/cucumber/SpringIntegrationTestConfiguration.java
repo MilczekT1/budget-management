6c42e784905bda2e2ad84c428c4d8275194de6eb
Konrad Boniecki <konrad_boniecki@hotmail.com>
2023-04-10 13:10:40 +0200

    .

--- budget-management-app/src/test/java/pl/konradboniecki/budget/budgetmanagement/cucumber/SpringIntegrationTestConfiguration.java
+++ budget-management-app/src/test/java/pl/konradboniecki/budget/budgetmanagement/cucumber/SpringIntegrationTestConfiguration.java
@@ -5,12 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Rule;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.cloud.config.environment.Environment;
+import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
-
-import java.util.Arrays;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
@@ -30,14 +28,11 @@ public class SpringIntegrationTestConfiguration {
     private static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0")
             .withExposedPorts(27017);
 
+    @Profile("test")
     @DynamicPropertySource
-    static void mongoDbProperties(DynamicPropertyRegistry registry, Environment environment) {
-        if (Arrays.asList(environment.getProfiles()).contains("acceptance-tests")) {
-            return;
-        } else {
-            mongoDBContainer.start();
-            registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
-        }
+    static void mongoDbProperties(DynamicPropertyRegistry registry) {
+        mongoDBContainer.start();
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
     }
 
 }
